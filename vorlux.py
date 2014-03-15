41a5d29db982fdbf4d9eda0ceb45353036114282
Brian Tomlinson <brian@smartm.no>
2014-03-15 14:43:24 +0100

    remove more duplicates

--- vorlux.py
+++ vorlux.py
@@ -68,14 +68,6 @@ def close_db(error):
         g.sqlite_db.close()
 
 
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
 def get_user_id(db, username):
     rv = db.execute('''SELECT userid FROM user WHERE username = ?''', [username], one=True)
     if rv:
