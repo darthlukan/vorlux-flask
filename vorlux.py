3109164ced0c29b53f20ee248ca1d1b9a16d48f9
anakin <anak1n@funtoo.org>
2014-03-15 05:26:31 -0700

    Got rid of uneeded modules

--- vorlux.py
+++ vorlux.py
@@ -125,23 +125,9 @@ def register():  # registering new user
     return render_template('register.html', error=error)
 
 @app.route('/')
-def show_entries():
-    db = get_db()
-    cur = db.execute('SELECT title, text FROM entries ORDER BY id DESC')
-    entries = cur.fetchall()
-    return render_template('base_layout.html', entries=entries)
+def home():
+    return render_template('base_layout.html')
 
-
-@app.route('/add', methods=['POST'])
-def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    db = get_db()
-    db.execute('INSERT INTO entries (title, text) VALUES (?, ?)',
-               [request.form['title'], request.form['text']])
-    db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -191,7 +177,7 @@ def donate():
 
 @app.route('/contact')
 def contact_us():
-	return render_template('conactus.html')
+	return render_template('contactus.html')
 
 if __name__ == '__main__':
     init_db()
