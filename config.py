43793fc1a2bfcc4520c7f711b5001e3135144c62
anakin <anak1n@funtoo.org>
2014-03-15 06:45:38 -0700

    Config.py finished for contacting support

--- config.py
+++ config.py
@@ -2,7 +2,14 @@
 
 from ConfigParser import SafeConfigParser
 
+secrets = SafeConfigParser()
+secrets.read('secrets.ini')
+
 MAIL_SERVER = 'smtp.google.com'
 MAIL_PORT = '587'
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
+MAIL_USERNAME = secrets.get("Mail", "USERNAME")
+MAIL_PASSWORD = secrets.get("Mail", "PASSWORD"
+
+ADMINS = ['support@vorluxhosting.com', 'anakin@vorluxhosting.com']
